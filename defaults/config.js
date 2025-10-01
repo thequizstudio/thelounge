8d24aee6697f7d88296a859b861f9a18fc162305
thequizstudio <jamestlampert@icloud.com>
2025-10-01 13:18:00 +0100

    Update config.js

--- defaults/config.js
+++ defaults/config.js
@@ -16,7 +16,7 @@ module.exports = {
 	//   channels and scrollbacks are available when they come back.
 	//
 	// This value is set to `false` by default.
-	public: false,
+	public: true,
 
 	// ### `host`
 	//
@@ -272,7 +272,7 @@ module.exports = {
 		nick: "thelounge%%",
 		username: "thelounge",
 		realname: "",
-		join: "#thelounge",
+		join: "#PubQuiz",
 		leaveMessage: "",
 	},
 
